256860ed1b524350dee8dec76d21730895c611f8
IrreplaceL <lirreplace@gmail.com>
2024-10-27 13:59:03 +0800

    第一次提交

---
+++ src/main/java/com/xianting/yoga/studio/XianTingYogaStudioApplication.java
@@ -0,0 +1,13 @@
+package com.xianting.yoga.studio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XianTingYogaStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XianTingYogaStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xianting/yoga/studio/XianTingYogaStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xianting.yoga.studio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XianTingYogaStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
