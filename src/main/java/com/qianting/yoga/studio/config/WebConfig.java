dd0e79e5e7a3cf04e5820c5aef53248c401ec36b
IrreplaceL <lirreplace@gmail.com>
2024-10-31 13:14:32 +0800

    将端口号6666修改后，apifox网页端接口测试通过

--- src/main/java/com/qianting/yoga/studio/config/WebConfig.java
+++ src/main/java/com/qianting/yoga/studio/config/WebConfig.java
@@ -24,6 +24,7 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("跨域已经启动");
         // 设置允许跨域的路径
         registry.addMapping("/**")
         // 设置允许跨域请求的域名
