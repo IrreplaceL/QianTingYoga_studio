28503cb27d8bff313c6a200ee631c31dbfbe117e
IrreplaceL <lirreplace@gmail.com>
2024-11-07 22:15:13 +0800

    评论表的crud操作

---
+++ src/main/java/com/qianting/yoga/studio/mapper/CommentMapper.java
@@ -0,0 +1,12 @@
+package com.qianting.yoga.studio.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.qianting.yoga.studio.domian.entity.Comment;
+
+/**
+ * 评论表，存储用户对文章的评论(Comment)表数据库访问层
+ *
+ * @author makejava
+ * @since 2024-11-07 21:54:09
+ */
+public interface CommentMapper extends BaseMapper<Comment> {
+}
